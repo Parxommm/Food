6dc6752b80db36df047eb719fdca25ee7b5c0f0b
Eugene <evg.parkhomenko@gmail.com>
2021-10-16 23:27:38 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -37,4 +38,59 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  // Timer
+  const deadline = '2021-12-31';
+
+  const getTimeRemaining = (endtime) => {
+    const remainsMs = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(remainsMs / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(remainsMs / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(remainsMs / (1000 * 60) % 60),
+          seconds = Math.floor(remainsMs / 1000 % 60);
+
+    return {
+      'total': remainsMs,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  };
+
+  const addZero = (num) => {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    }
+    return `${num}`;
+  };
+
+  const setClock = (selector, endtime) => {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds');
+
+    const updateClock = () => {
+      const remainsArr = getTimeRemaining(endtime);
+
+      days.innerHTML = addZero(remainsArr.days);
+      hours.innerHTML = addZero(remainsArr.hours);
+      minutes.innerHTML = addZero(remainsArr.minutes);
+      seconds.innerHTML = addZero(remainsArr.seconds);
+
+      if (remainsArr.total <= 0) {
+        clearInterval(timerInterval);
+      }
+    };
+
+    updateClock();
+
+    const timerInterval = setInterval(updateClock, 1000);
+  };
+  
+  setClock('.timer', deadline);
+
 });
+
